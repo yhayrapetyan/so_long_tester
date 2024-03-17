db23394a9882f8f0d9104015c96a3bb04c02cc1e
Yura-VM <211003yura@gmail.com>
2024-03-17 12:56:32 +0400

    optimized the script, still only for linux

--- test.sh
+++ test.sh
@@ -11,34 +11,66 @@ CYAN='\033[0;96m'
 GRAY='\033[0;90m'
 WHITE='\033[0;97m'
 
+action=$1
+default_action="default"
+
+# Check if an argument is provided
+if [ $# -eq 1 ]; then
+    action=$1
+else
+    action=$default_action
+fi
+
+
+# Perform different actions based on the argument
+case $action in
+    clean)
+        rm -rf Test_results
+        printf ${BLUE}"\n-----------------------\n"${DEF_COLOR};
+        printf ${GREEN}"  Test_results deleted"${DEF_COLOR};
+        printf ${BLUE}"\n-----------------------\n\n"${DEF_COLOR};
+        exit 0
+        ;;
+    default)
+        ;;
+    *)
+        printf "${RED}Unknown action: ${YELLOW}$action\n${DEF_COLOR}"
+        printf "${BLUE}Valid actions: ${GREEN}clean\n${DEF_COLOR}"
+        exit 1
+        ;;
+esac
+
 printf ${BLUE}"\n-------------------------------------------------------------\n"${DEF_COLOR};
 printf ${YELLOW}"\n\t\tTEST CREATED BY: "${DEF_COLOR};
 printf ${CYAN}"Me :)\t\n"${DEF_COLOR};
 printf ${BLUE}"\n-------------------------------------------------------------\n"${DEF_COLOR};
 
+
+
 rm -rf Test_results
 
-FILE=$PWD/../so_long
+PROGRAM=$PWD/../so_long
 INVALID_MAPS="$PWD/Test_maps/invalid"
 OUTPUT="$PWD/Test_results"
-TESTER=$PWD/check_leaks
 ERRORS_COUNT=0
 
-if [ -f "$FILE" ]; then
+make -C "$PWD/../" re
+
+if [ -f "$PROGRAM" ]; then
 	echo -n
 else
 	printf "${RED}SO_LONG PROGRAM DOES NOT EXISTS${DEF_COLOR}\n";
 	exit 1
 fi
 
+
 mkdir Test_results
 
-cc $TESTER.c get_next_line.c get_next_line_utils.c -o check_leaks
 
 subdirs=$(ls -d "$INVALID_MAPS"/* 2>/dev/null)
-
 # Iterate over each subdirectory
 for subdir in $subdirs; do
+    test_number=1
     subdir_name=$(basename "$subdir")
     printf "${MAGENTA}\n-------------------------------------------------------------\n${DEF_COLOR}"
     printf "${CYAN}                         $subdir_name\n"
@@ -47,16 +79,16 @@ for subdir in $subdirs; do
     for file_path in "$subdir"/*; do
         if [ -f "$file_path" ]; then
             file_name="${file_path##*/}"
-            valgrind --leak-check=full "$FILE" "$file_path" > $OUTPUT/$subdir_name/$file_name 2>&1
-            $TESTER "$OUTPUT/$subdir_name/$file_name"
-            valgrind_exit_status=$?
-            "$FILE" "$file_path"
-            if [ $valgrind_exit_status -eq 0 ]; then
+            printf  "${CYAN}Test ${test_number}\n${DEF_COLOR}"
+            valgrind_exit_status=$(valgrind --leak-check=full --show-leak-kinds=all --log-fd=1 $PROGRAM $file_path | grep -Ec 'no leaks are possible|ERROR SUMMARY: 0')
+            valgrind --leak-check=full --show-leak-kinds=all "$PROGRAM" "$file_path" > $OUTPUT/$subdir_name/$file_name 2>&1
+            if [[ $valgrind_exit_status == 2 ]]; then
                 printf "${GREEN}[OK LEAKS] ${DEF_COLOR}\n";
             else
                 printf "${RED} [KO LEAKS] ${DEF_COLOR}\n";
                 ((ERRORS_COUNT++))
             fi
+            ((test_number++))
         fi
     done
 done
@@ -66,6 +98,3 @@ if [ "$ERRORS_COUNT" -ne 0 ]; then
 else
     printf "${GREEN}\nGood job! Everything works correctly${DEF_COLOR}\n\n"
 fi
-
-rm -rf $TESTER
-
